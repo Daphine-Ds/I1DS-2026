48f22c4469128959eabdd3ed9060a6a12d71985c
Daphine-Ds <sivieridaphine069@gmail.com>
2026-03-24 16:57:58 -0300

    Lista de exercicios 50 atividades variáveis

---
+++ atividades/09-atividades50.js
@@ -0,0 +1,62 @@
+/*PARTE 1 – Variáveis, Operações e Lógica (1 a 10)
+1. Crie variáveis para nome, idade e cidade. Exiba uma frase completa.
+2. Crie duas variáveis numéricas e exiba:
+      soma
+      subtração
+      multiplicação
+      divisão
+      resto da divisão
+3. Calcule a área de um retângulo.
+4. Calcule o volume de um paralelepípedo.
+5. Dado o valor de um produto, calcule: 10% de desconto | valor final
+6. Converta temperatura de Celsius para Fahrenheit.
+7. Calcule o IMC e exiba o valor.
+8. Calcule a média de 3 notas.
+9. Calcule o delta de uma equação de 2º grau.
+10. Dado um valor em horas, converta para minutos e segundos.*/
+
+//Criando variáveis com "var" (var nome_variavel = <valor>;)
+var nome = "Daphine";
+
+//Criando variável de idade e atribuindo uma idade para ela
+var idade = 16;
+
+//Criando variável de cidade e atribuindo uma cidade pra ela
+var cidade = "Jaú";
+
+//Demonstrando no console várias informações
+console.log(
+  "Meu nome é",
+  nome,
+  "eu tenho",
+  idade,
+  "anos",
+  "e moro em",
+  cidade,
+  ".",
+);
+
+let x = 20;
+let y = 4;
+
+console.log(
+  "-----------------------------------------------------------------------",
+);
+
+// Criando variáveis numéricas
+console.log("X = ", x, "| Y = ", y);
+
+resultado = x + y;
+console.log("A soma é", resultado);
+
+resultado = x - y;
+console.log("A subtração é", resultado);
+
+resultado = x * y;
+console.log("A multiplicação é", resultado);
+
+resultado = x / y;
+console.log("A divisão é", resultado);
+
+
+//
---
+++ atividades/10-atividades50parte2.js
@@ -0,0 +1,35 @@
+/*PARTE 2 – Condicionais (IF) (11 a 20)
+11. Verifique se um número é positivo, negativo ou zero.
+12. Verifique se um número é par ou ímpar.
+13. Dado um número, verifique se é múltiplo de 3.
+14. Verifique se um aluno foi:
+       Aprovado (≥ 7)
+       Recuperação (≥ 5 e < 7)
+       Reprovado (< 5)
+15. Verifique se uma pessoa pode votar (use regras reais).
+16. Verifique se um número está entre 10 e 50.
+17. Crie um sistema de login simples (usuário e senha).
+18. Verifique o maior entre dois números.
+19. Verifique o maior entre três números.
+20. Classifique a idade:
+       Criança
+       Adolescente
+       Adulto
+       Idoso
+*/
+
+// Verifique se é o número positivo ou negativo
+let numero = -5;
+if (numero > 0) {
+  console.log("O número é positivo.");
+}
+
+if (numero < 0) {
+  console.log("O número é negativo.");
+}
+if (numero == 0) {
+  console.log("O número é zero.");
+}
+
+// Verifique se o número é ímpar ou par
+
