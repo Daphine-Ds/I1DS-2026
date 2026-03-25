512b6edde2cee11a2b87b9cab2907be5298fea62
Daphine-Ds <sivieridaphine069@gmail.com>
2026-03-25 11:19:51 -0300

    atividades50 parte 1  variaveis

--- atividades/09-atividades50.js
+++ atividades/09-atividades50.js
@@ -9,13 +9,13 @@
 3. Calcule a área de um retângulo.
 4. Calcule o volume de um paralelepípedo.
 5. Dado o valor de um produto, calcule: 10% de desconto | valor final
-6. Converta temperatura de Celsius para Fahrenheit.
+6. Converta temperatura de Celsius para  Fahrenheit.
 7. Calcule o IMC e exiba o valor.
 8. Calcule a média de 3 notas.
 9. Calcule o delta de uma equação de 2º grau.
 10. Dado um valor em horas, converta para minutos e segundos.*/
 
-//Criando variáveis com "var" (var nome_variavel = <valor>;)
+// 1.Criando variáveis com "var" (var nome_variavel = <valor>;)
 var nome = "Daphine";
 
 //Criando variável de idade e atribuindo uma idade para ela
@@ -43,7 +43,7 @@ console.log(
   "-----------------------------------------------------------------------",
 );
 
-// Criando variáveis numéricas
+// 2.Criando variáveis numéricas
 console.log("X = ", x, "| Y = ", y);
 
 resultado = x + y;
@@ -57,6 +57,102 @@ console.log("A multiplicação é", resultado);
 
 resultado = x / y;
 console.log("A divisão é", resultado);
+console.log("---------------------------------------------------");
+
+// 3.Calcule a área de um triangulo
+let base = 10;
+let hTriangulo = 5;
+
+let area = (base * hTriangulo) / 2;
+
+console.log("A base do triângulo é:", base);
+console.log("A altura do triângulo é:", hTriangulo);
+console.log("Então a área do triângulo é:", area);
+console.log("---------------------------------------------------");
+
+// 4.Calcule o volume do paralelepípedo
+let comprimento = 10;
+let largura = 5;
+let hParalelepipedo = 3;
+
+let volumeP = comprimento * largura * hParalelepipedo;
+
+console.log("O comprimento é:", comprimento);
+console.log("A largura é", largura);
+console.log("A altura é:", hParalelepipedo);
+console.log("O Volume do paralelepípedo é:", volumeP);
+console.log("---------------------------------------------------");
+
+// 5.Calcule 10% do desconto
+let valor = 200;
+let multiplicacao = 10;
+let divisao = 100;
+
+let desconto = (valor * multiplicacao) / divisao;
+
+console.log("O valor do produto é R$:", valor, "reais");
+console.log(
+  "Com o desconto de 10% o produto saira por R$:",
+  valor - desconto,
+  "reais",
+);
+console.log("---------------------------------------------------");
+
+// 6.Converta a temperatura de Celcius para  Fahrenheit.
+let celcius = 20;
+
+let fahrenheit = celcius * 1.8 + 32;
+
+console.log(
+  "A temperatura de",
+  celcius,
+  "graus celcius",
+  "em fahrenheit é",
+  fahrenheit,
+);
+console.log("---------------------------------------------------");
+
+// 7.Calcule o IMC
+let peso = 70;
+let altura = 1.7;
+
+let IMC = peso / (altura * altura);
+
+console.log("O seu peso é:", peso);
+console.log("A sua altura é:", altura);
+console.log("Então o seu IMC é:", IMC);
+console.log("-----------------------------------------------");
+
+// 8.Calculando a média
+let n1 = 8;
+let n2 = 10;
+let n3 = 6;
+
+let media = (n1 + n2 + n3) / 4;
+
+console.log("Nota da primeira prova:", n1);
+console.log("Nota da segunda prova:", n2);
+console.log("Nota da terceira prova:", n3);
+console.log("A sua média é:", media);
+console.log("---------------------------------------------------");
+
+// 9.Calcule o valor de delta de uma equção de 2° grau
+let ax2 = 1;
+let b = -6;
+let c = 5;
+
+let delta = (b * b) - (4 * ax2 * c);
+
+console.log("Valor de ax²:", ax2);
+console.log("Valor de b:", b);
+console.log("Valor de c:", c)
+console.log("O valor de delta é:", delta)
+console.log("---------------------------------------------------");
+
+// 10.Dado o valor em hora converta pra minutos e segundos 
+let horas = 2
+let minutos = (horas * 60)
+let segundos = (horas * 3600)
 
 
-//
+
