e3d72180d237e77ab6386e79a4fce94da8175ff5
Daphine-DS <sivieridaphine069@gmail.com>
2026-04-29 13:30:55 -0300

    Exercicios envolvendo JavaScript, Html e CSS

--- Atividade/js/script.js
+++ Atividade/js/script.js
@@ -1,15 +1,17 @@
-let userCount = 0;
+let userCount = 0; // Contador
 
 const cadastrar = (event) => {
   event.preventDefault();
+
+  // Capturar os valores do elemento por Id
 
   let nome = document.getElementById("nome").value;
   let email = document.getElementById("email").value;
 
   let lista = document.getElementById("lista"); 
 
-  userCount++;
-  let id = userCount;
+  userCount++; // Acrecser o contador em +1
+  let id = userCount; // Criar um novo elemento  <li>Nome - Email</li>
 
   let novoItem = document.createElement("li");
   novoItem.setAttribute("data-id", id);
@@ -17,6 +19,8 @@ const cadastrar = (event) => {
   novoItem.innerHTML = id + " - " + nome + " - " + email + 
   ` <button onclick="editar(${id})">Editar</button>
     <button onclick="excluir(${id})">Excluir</button>`;
+
+    // Adicionar um novo elemento na lista ja existente
 
   lista.appendChild(novoItem);
 
@@ -26,7 +30,16 @@ const cadastrar = (event) => {
 
 const editar = (id) => {
   console.log("Editar:", id);
+
+  itens.forEach((item) => {
+    if (item.getAttribute("data-id") == id) {
+      item.editar();
+    }
+  });
 }
+
+
+// Função excluir
 
 const excluir = (id) => {
   const itens = document.querySelectorAll("li");
